c159d2901fbf1552f90248d820ea80c7883442ce
Shamsul Majid <66409646+ShmslMjd@users.noreply.github.com>
2021-03-30 17:09:00 +0800

    new files
    new file go brrrrrrrrr

---
+++ unity/FYP-Unity-Project/Assets/Script/CharacterItemUI.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+public class CharacterItemUI : MonoBehaviour
+{
+    [SerializeField] Color itemNotSelectedColor;
+    [SerializeField] Color itemSelectedColor;
+
+    [Space(20f)]
+    [SerializeField] Image characterImage;
+    [SerializeField] TMP_Text characterNameText;
+    [SerializeField] TMP_Text characterPriceText;
+    [SerializeField] Button characterPurchaseButton;
+
+    [Space(20f)]
+    [SerializeField] Button ItemButton;
+    [SerializeField] Image ItemImage;
+    [SerializeField] Outline ItemOutline;
+
+    //--------------------------------------//
+
+    public void SetItemPosition(Vector2 pos)
+    {
+        GetComponent<RectTransform>().anchoredPosition += pos;
+    }
+
+    public void SetCharacterImage(Sprite sprite)
+    {
+        characterImage.sprite = sprite;
+    }
+
+    public void SetCharacterName(string name)
+    {
+        characterNameText.text = name;
+    }
+
+    public void SetCharacterPrice(int price)
+    {
+        characterPriceText.text = price.ToString();
+    }
+
+    public void SetCharacterAsPurchased()
+    {
+        characterPurchaseButton.gameObject.SetActive(false);
+        ItemButton.interactable = true;
+
+        ItemImage.color = itemNotSelectedColor;
+    }
+
+    public void OnItemPurchase(int itemIndex, UnityAction<int> action)
+    {
+        characterPurchaseButton.onClick.RemoveAllListeners();
+        characterPurchaseButton.onClick.AddListener (() => action.Invoke (itemIndex));
+    }
+
+    public void OnItemSelect(int itemIndex, UnityAction<int> action)
+    {
+        ItemButton.interactable = true;
+        ItemButton.onClick.RemoveAllListeners();
+        ItemButton.onClick.AddListener(() => action.Invoke(itemIndex));
+    }
+
+    public void SelectItem()
+    {
+        ItemOutline.enabled = true;
+        ItemImage.color = itemSelectedColor;
+        ItemButton.interactable = false;
+    }
+
+    public void DeselectItem()
+    {
+        ItemOutline.enabled = false;
+        ItemImage.color = itemNotSelectedColor;
+        ItemButton.interactable = true;
+    }
+
+}
---
+++ unity/FYP-Unity-Project/Assets/Script/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddWater();
+
+    }
+
+    void AddWater()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameDataManager.AddCoins(100);
+        }
+
+
+        GameSharedUI.Instance.UpdateCoinsUIText();
+    }
+
+}
