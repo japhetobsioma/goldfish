89a0bd4f4a1b0c8c09b72c7b3cc0546111e70a61
Shamsul Majid <66409646+ShmslMjd@users.noreply.github.com>
2021-04-13 13:13:26 +0800

    Camera Control
    Camera interactable

---
+++ unity/FYP-Unity-Project/Assets/Script/CameraOrbit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+
+    public float rotationSpeed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    public void Rotate()
+    {
+        float h =  rotationSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+        transform.Rotate(0, h, 0);
+    }
+}
