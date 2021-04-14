1fa413c59a0151758c05120b331a384cc1b0329b
Shamsul Majid <66409646+ShmslMjd@users.noreply.github.com>
2021-04-14 13:01:54 +0800

    Camera Fixed
    - User can rotate the camera in X-axis and Y axis
    - Limit the user rotation

--- unity/FYP-Unity-Project/Assets/Script/CameraOrbit.cs
+++ unity/FYP-Unity-Project/Assets/Script/CameraOrbit.cs
@@ -6,16 +6,30 @@ public class CameraOrbit : MonoBehaviour
 {
 
     public float rotationSpeed = 5.0f;
+    private float xRotation;
+    private float yRotation;
+    private float minAngle = 0.0f;
+    private float maxAngle = 40.0f;
 
     // Update is called once per frame
     void Update()
     {
-        Rotate();
+        if (Input.GetMouseButton(0))
+        {
+
+            yRotation -= Input.GetAxis("Mouse Y") * rotationSpeed;
+            xRotation += Input.GetAxis("Mouse X") * rotationSpeed;
+            yRotation = Mathf.Clamp(yRotation, minAngle, maxAngle);
+            transform.eulerAngles = new Vector3(yRotation, xRotation, 0.0f);
+
+            /**
+            transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * rotationSpeed, Input.GetAxis("Mouse X") * rotationSpeed, 0));
+            xRotation = transform.rotation.eulerAngles.x;
+            yRotation = transform.rotation.eulerAngles.y;
+            xRotation = Mathf.Clamp(xRotation, minAngle, maxAngle);
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            **/
+        }
     }
 
-    public void Rotate()
-    {
-        float h =  rotationSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
-        transform.Rotate(0, h, 0);
-    }
 }
